f9a1e33c58a48a6a295d7adfe5a42ae56addada8
skynowa <skynowa@gmail.com>
2015-10-19 17:50:21 +0300

    Config: IS_SYSTEM_TRAY_ICON_MESSAGES = 0

--- Config.h
+++ Config.h
@@ -116,5 +116,5 @@ const TableViewHeader tableViewHeaders[] = {
 #define BUTTON_BOX_WIDTH         75
 #define TERM_MINIMIZED_SIZE_MAX  30
 #define QSYSTEM_TRAYICON_MESSAGE_TIMEOUT_MSEC 5000
-#define IS_SYSTEM_TRAY_ICON_MESSAGES 1
+#define IS_SYSTEM_TRAY_ICON_MESSAGES 0
 //-------------------------------------------------------------------------------------------------
