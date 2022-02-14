1ba204a46e24f94d3efe3256eea0b609ba1d019f
skynowa <skynowa@gmail.com>
2022-02-14 15:25:04 +0200

    SqliteDb.cpp [2+/1-] - test(s)

--- Classes/SqliteDb.cpp
+++ Classes/SqliteDb.cpp
@@ -23,7 +23,8 @@ SqliteDb::SqliteDb(
     _navigator(a_parent)
 {
     qTEST_PTR(a_parent);
-    qTEST(!a_filePath.isEmpty())
+    qTEST(!a_filePath.isEmpty());
+    qTEST(QDir(a_filePath).exists());
 }
 //-------------------------------------------------------------------------------------------------
 SqliteDb::SqliteDb(
