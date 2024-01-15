ea8fa793a99989fc5b15ecd87b16df43c957f588
skynowa <skynowa@gmail.com>
2024-01-16 01:20:37 +0200

    SqliteDb.cpp [1+/1-] - comment(s)

--- Classes/SqliteDb.cpp
+++ Classes/SqliteDb.cpp
@@ -337,7 +337,7 @@ SqliteDb::_dbOpen()
                 "    " DB_F_MAIN_VALUE      " varchar(255) DEFAULT '', "
                 "    " DB_F_MAIN_IS_LEARNED " integer      DEFAULT 0, "
                 "    " DB_F_MAIN_IS_MARKED  " integer      DEFAULT 0, "
-                "    " DB_F_MAIN_TAG        " integer      DEFAULT 1, "
+                "    " DB_F_MAIN_TAG        " integer      DEFAULT 1, " // 1 - Empty string
                 " "
                 "    FOREIGN KEY (" DB_F_MAIN_TAG ") REFERENCES " DB_T_TAGS "(" DB_F_TAGS_ID ") "
                 "    ON UPDATE CASCADE "
